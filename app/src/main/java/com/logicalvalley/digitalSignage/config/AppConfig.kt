7136f1b631b5e96e4cc263eb5ee2b6616c29cd36
Sumit Yadav <sumits7y@gmail.com>
2025-12-29 23:00:16 +0530

    added prod url

--- app/src/main/java/com/logicalvalley/digitalSignage/config/AppConfig.kt
+++ app/src/main/java/com/logicalvalley/digitalSignage/config/AppConfig.kt
@@ -1,6 +1,6 @@
 package com.logicalvalley.digitalSignage.config
 
 object AppConfig {
-    const val BASE_URL = "http://10.0.2.2:3000"
+    const val BASE_URL = "https://signage.logicalvalley.in"
 }
 
