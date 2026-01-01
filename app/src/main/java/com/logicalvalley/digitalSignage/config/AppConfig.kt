7cf9e30cc1cb10e934eeeb66ba39ffd156fc3998
Sumit Yadav <sumits7y@gmail.com>
2026-01-01 23:28:56 +0530

    added urls

--- app/src/main/java/com/logicalvalley/digitalSignage/config/AppConfig.kt
+++ app/src/main/java/com/logicalvalley/digitalSignage/config/AppConfig.kt
@@ -5,6 +5,7 @@ object AppConfig {
      * Use "http://10.0.2.2:3000" for Android Emulator to reach host's localhost.
      * Use your PC's IP (e.g., "http://192.168.x.x:3000") for physical devices.
      */
-    const val BASE_URL = "http://10.0.2.2:3000"
+//    const val BASE_URL = "http://10.0.2.2:3000"
+    const val BASE_URL = "https://signage.logicalvalley.in"
 }
 
