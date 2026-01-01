c8b32c7b5b164b718653b7c477be58a672f24ce6
Sumit Yadav <sumits7y@gmail.com>
2026-01-01 21:07:54 +0530

    App ready part 2

--- app/src/main/java/com/logicalvalley/digitalSignage/config/AppConfig.kt
+++ app/src/main/java/com/logicalvalley/digitalSignage/config/AppConfig.kt
@@ -1,6 +1,6 @@
 package com.logicalvalley.digitalSignage.config
 
 object AppConfig {
-    const val BASE_URL = "https://signage.logicalvalley.in"
+    const val BASE_URL = "http://localhost:3000"
 }
 
