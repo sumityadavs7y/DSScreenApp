fc7c135332173c7db6da380559946ff2a93a68e6
Sumit Yadav <sumits7y@gmail.com>
2026-04-04 12:07:47 +0530

    version inc

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.logicalvalley.digitalSignage"
         minSdk = 23
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
     }
 
